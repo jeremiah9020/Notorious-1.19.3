9be3048c92c83a6c165953686c711726895cb166
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-01-20 11:22:15 -0800

    Initial commit

---
+++ src/client/java/net/jabberjerry/notorious/NotoriousClient.java
@@ -0,0 +1,10 @@
+package net.jabberjerry.notorious;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotoriousClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
